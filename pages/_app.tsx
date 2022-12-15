8e39fb388baff8dc1f9a6fd2e3c3876794dd7d11
MariuzM <mariusdotdev@gmail.com>
2022-12-16 00:01:25 +0700

    i18n init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { appWithTranslation } from 'next-i18next';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default appWithTranslation(App);
