db0cf44426f82164562d8c8ec58ba35f329f17df
MariuzM <mariusdotdev@gmail.com>
2022-12-16 01:50:41 +0700

    config for new path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,15 @@
+const path = require('path');
+
 module.exports = {
   debug: true,
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de'],
   },
+  // ns: [],
+  // defaultNS: '',
+
+  localePath: path.resolve('public/locales'),
+  localeStructure: '{{lng}}',
+  localExtension: null,
 };
